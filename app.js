0b2979cc81cd978916e9f092f2126f4fa49cb0e6
Amrit Shenava <amritshenava@outlook.com>
2019-06-03 02:04:19 -0400

    Update

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 import * as PIXI from 'pixi.js';
+
+const canvas = document.getElementById('canvas');
+const app = new PIXI.Application({
+  view : canvas,
+  width : window.innerWindow,
+  height: window.innerHeight
+});
+
+const texture = PIXI.Texture.from('sprite.png');
+const img = new PIXI.Sprite(texture);
+img.x = app.renderer.width / 2;
+img.y = app.renderer.height / 2;
+app.stage.addChild(img); 
+
+
+export default app;
